62773822424c5f13b4e3110a0bf0866bf97a5e65
navincograph179 <navin_gurung@co-graph.com>
2025-02-11 23:12:39 +0900

    update readme

--- codes/04_backend-professional-project/src/db/index.js
+++ codes/04_backend-professional-project/src/db/index.js
@@ -4,8 +4,8 @@ import {DB_NAME} from "../constants.js"
 
 const connectDB = async () => {
   try {
-      await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-      console.log("Database COnnected");
+      const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+      console.log(`\n Mongodb connected!!! DB Host: ${connectionInstance.connection.host}`);
       
   } catch (error) {
     console.log(error.message)
